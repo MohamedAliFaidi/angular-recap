73b7c588c7dffc754650a08d4d06f04ce012ebca
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-02-03 10:02:51 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormComponent } from './form/form.component';
 import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FormComponent,
-    DetailsComponent
+    DetailsComponent,
+    ListComponent
     
   ],
   imports: [
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent {
+  @Input() messages : Array<any> = []
+
+}
