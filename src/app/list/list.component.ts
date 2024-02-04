f501de0614a700eb5a8a914cd0b294cc7982537f
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-02-04 14:30:07 +0100

    type

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TMessage } from '../../types/type';
 
 @Component({
   selector: 'app-list',
@@ -8,7 +9,7 @@ import { Component, Input } from '@angular/core';
 
 
 export class ListComponent {
-  @Input() messages: Array<any> = []
+  @Input() messages: Array<TMessage> = []
 
 
   del(index: number | string) {
