d66dcc27538bfee4e946a64e852f40dc620e53e8
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-02-09 17:08:12 +0100

    playing with query params in the about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -4,31 +4,25 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent implements OnInit {
-  constructor(
-    private activatedRoute : ActivatedRoute
-  ){}
-  param : string  ="";
-  query_1  :string ="";
-  query_2: string ="";
+  constructor(private activatedRoute: ActivatedRoute) {}
+  param: string = '';
+  querys: any[] = [];
   ngOnInit(): void {
-     //getting the params username from url look at routing module
-    this.param = this.activatedRoute.snapshot.params['username']
-
-
+    //getting the params username from url look at routing module
+    this.param = this.activatedRoute.snapshot.params['username'];
     //getting the query  params from the url
     // you can test this exemple
     //"http://localhost:4200/about/daly?query_1=someFirstQueryParam&query_2=someSecondQueryParam"
-    this.query_1 = this.activatedRoute.snapshot.queryParams['query_1']
-    this.query_2 = this.activatedRoute.snapshot.queryParams['query_2']
+    //  Object.entries(this.activatedRoute.snapshot.queryParams).forEach((e)=>[
+    //     console.log(e)
+    //   ])
 
-
-    
-
-    
-
+    // playing with querys
+    Object.entries(this.activatedRoute.snapshot.queryParams).forEach((item) => {
+      this.querys.push(item);
+    });
   }
-
 }
