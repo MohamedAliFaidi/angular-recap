b0fc21444d229719648c33a9783e1f4763864866
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-02-10 11:30:43 +0100

    providing http to the angular server

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
 
 
 import { AppModule } from './app.module';
@@ -14,5 +16,7 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent
   ],
+  providers: [provideHttpClient(withFetch())]
+
 })
 export class AppServerModule {}
